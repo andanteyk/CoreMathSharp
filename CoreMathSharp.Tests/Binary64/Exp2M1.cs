fea9a7a813515218b81151d97b143404b6c7a893
Andante <andante.soft@gmail.com>
2026-01-28 10:14:06 +0900

    exp2m1

--- CoreMathSharp.Tests/Binary64/Exp2M1.cs
+++ CoreMathSharp.Tests/Binary64/Exp2M1.cs
@@ -2,21 +2,22 @@ using System.Globalization;
 
 namespace CoreMathSharp.Tests;
 
-public class Exp2
+public class Exp2M1
 {
     [Fact]
     public void TestDoubles()
     {
         foreach (var x in Helper.TestDoubles)
         {
-            double expected = double.Exp2(x);
-            double actual = StrictMath.Exp2(x);
+            double expected = double.Exp2M1(x);
+            double actual = StrictMath.Exp2M1(x);
             double ulp = Math.Max(Math.BitIncrement(actual) - actual, actual - Math.BitDecrement(actual));
 
             Assert.Equal(expected, actual, double.IsNaN(ulp) ? 0.0 : ulp);
         }
     }
 
+    // The current implementation of Exp2M1() in .NET is `Pow(2, x) - 1`, which has a very large error. 
     [Fact]
     public void Random()
     {
@@ -24,30 +25,32 @@ public class Exp2
 
         for (int i = 0; i < 1024 * 1024; i++)
         {
-            double x = rng.NextDouble(-1024.0, 1024.0);
-            double expected = double.Exp2(x);
-            double actual = StrictMath.Exp2(x);
+            double x = rng.NextDouble(-64.0, 1024.0);
+            double expected = double.Exp2M1(x);
+            double actual = StrictMath.Exp2M1(x);
             double ulp = Math.Max(Math.BitIncrement(actual) - actual, actual - Math.BitDecrement(actual));
+            double torelance = Math.Max(double.IsNaN(ulp) ? 0.0 : ulp, 8.8817841970012523e-16);
 
-            Assert.Equal(expected, actual, double.IsNaN(ulp) ? 0.0 : ulp);
+            Assert.Equal(expected, actual, torelance);
         }
 
         for (int i = 0; i < 1024 * 1024; i++)
         {
             double x = Polyfill.UInt64BitsToDouble(rng.Next());
 
-            double expected = double.Exp2(x);
-            double actual = StrictMath.Exp2(x);
+            double expected = double.Exp2M1(x);
+            double actual = StrictMath.Exp2M1(x);
             double ulp = Math.Max(Math.BitIncrement(actual) - actual, actual - Math.BitDecrement(actual));
+            double torelance = Math.Max(double.IsNaN(ulp) ? 0.0 : ulp, 8.8817841970012523e-16);
 
-            Assert.Equal(expected, actual, double.IsNaN(ulp) ? 0.0 : ulp);
+            Assert.Equal(expected, actual, torelance);
         }
     }
 
     [Fact]
     public void TestVector()
     {
-        string path = "../../../Binary64/exp2.txt";
+        string path = "../../../Binary64/exp2m1.txt";
 
         foreach (var line in File.ReadLines(path))
         {
@@ -56,7 +59,7 @@ public class Exp2
             double x = Polyfill.UInt64BitsToDouble(ulong.Parse(parsed[0], NumberStyles.HexNumber));
             double a = Polyfill.UInt64BitsToDouble(ulong.Parse(parsed[1], NumberStyles.HexNumber));
 
-            double actual = StrictMath.Exp2(x);
+            double actual = StrictMath.Exp2M1(x);
             Assert.Equal(a, actual);
         }
     }
